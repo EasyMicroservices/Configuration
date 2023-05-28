514f6bb88dd34ef11fb4b53e06c0f9b771152390
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-28 16:44:38 +0330

    add base provider stuff

---
+++ src/CSharp/EasyMicroservices.Configuration/Providers/BaseConfigProvider.cs
@@ -0,0 +1,39 @@
+﻿using EasyMicroservices.Configuration.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.Configuration.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class BaseConfigProvider : IConfigProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public abstract Task<string> GetValue(string key);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public abstract Task SetValue(string key, string value);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<T> GetValue<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
